b96991f69ed58329a029f5f2795209d10f1c9af0
ante237 <anton.helmersson00@gmail.com>
2025-07-08 13:46:40 +0200

    Setup sender

---
+++ sender.py
@@ -0,0 +1,47 @@
+import socket
+import sys
+import time
+
+udp_ip = "127.0.0.1"
+udp_port = 5005
+TEST_MSG  = 'A'*1465
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+def setupReciever(ip = "127.0.0.1", port = 5005):
+    print("Set up reciever")
+    global udp_ip
+    global udp_port
+    udp_ip = ip
+    udp_port = port
+
+def streamData(frequency: int, duration: int, msg: str) -> int:
+    '''
+    Stream data to reciever.
+    
+    :param int frequency: The number of times per second to send data
+    :param int duration: The duration over which to send data
+    :param string str: The message to be sent
+    :return int: Response code
+    '''
+    before = time.time()
+    sleepTime = 1 / frequency
+    pckCount = 0
+
+    print(f"Begin sending packages to {udp_ip}...")
+
+    for i in range(duration*frequency):
+        pckCount  = pckCount + 1
+        payload = str(10000 + pckCount) + ';' + msg + "####"
+        sock.sendto(payload.encode(), (udp_ip, udp_port))
+        time.sleep(sleepTime)
+
+    print(f"Sent {frequency * duration} packages in {round(time.time() - before, 2)} seconds")
+    return 0
+
+if __name__ == "__main__":
+    print(sys.argv)
+    if(len(sys.argv) > 2):
+        setupReciever(sys.argv[1], int(sys.argv[2]))
+
+    streamData(10, 20, TEST_MSG)
